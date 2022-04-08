9bca81b6abd9c4e1373709f8ecf0b451be96ea24
Sean Choi <ysc@purdue.edu>
2022-04-07 21:08:15 -0400

    Pi approximator using the leibniz formula

---
+++ pi_approximate.c
@@ -0,0 +1,24 @@
+#include "pi_approximate.h"
+
+#include <stdio.h>
+
+double sum_getter(int denominator) {
+	return (1.0/denominator);
+}
+
+int main() {
+	double sum = 0.0;
+	int counter = 1;
+
+	for (int i = 0; i >= 0; i++) {
+		if (i % 2 == 0) {
+			sum += sum_getter(counter);
+		}
+		else {
+			sum -= sum_getter(counter);
+		}
+		counter += 2;
+		
+		printf("%f\n", sum * 4);
+	}
+}
---
+++ pi_approximate.h
@@ -0,0 +1,6 @@
+#ifndef PI_APPROXIMATE_H
+#define PI_APPROXIMATE_H
+
+double sum_getter(int);
+
+#endif // PI_APPROXIMATE_H
